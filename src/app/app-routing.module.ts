4dc61ef20b2e69c5710e8fd28af64a705fe97a25
Jake Jeon <104756099+projjk@users.noreply.github.com>
2022-05-04 01:02:39 -0600

    Changed the default home page to menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent} from "./menu/pages/home/home.component";
+import {MenuComponent} from "./menu/pages/menu/menu.component";
 
 const routes: Routes = [
   { path: 'manager', loadChildren: () => import('./manager/manager.module').then((m) => m.ManagerModule) },
-  { path: '', component: HomeComponent },
+  { path: '', component: MenuComponent },
   { path: '**', redirectTo: "/" }
 ];
 
--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MenuComponent} from "./pages/menu/menu.component";
 import {CartComponent} from "./pages/cart/cart.component";
+import {HomeComponent} from "./pages/home/home.component";
 
 const routes: Routes = [
+   {path: 'home', component: HomeComponent},
    {path: 'menu', component: MenuComponent},
    {path: 'menu/:id', component: MenuComponent},
    {path: 'cart', component: CartComponent}
